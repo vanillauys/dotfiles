a754c28b672a2d4079b2ac71de262ed9f0e4f72b
Wihan Uys <wihan@glydepay.io>
2024-04-18 01:15:04 +0200

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Get the list of items in the .dotfiles directory
-items=$(ls | grep -v -e "README.md" -e "installs" -e "run.sh")
+items=$(ls | grep -v -e "README.md" -e "installs" -e "run.sh" -e "update")
 
 # Use gum filter to select items
 selected_items=$(echo "$items" | gum choose --no-limit)
